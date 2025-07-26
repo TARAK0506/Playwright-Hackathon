4919b3b2387551d6cd251990f71ec74b69a08ce6
TARAKA RAMARAO <tarakaramarao0506@gmail.com>
2025-07-26 18:04:24 +0530

    configuration modified

--- playwright.config.js
+++ playwright.config.js
@@ -13,27 +13,63 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: './tests',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  // reporter: [
+  //   ['html'],
+  //   ['allure-playwright', { outputFolder: 'reports/allure-reports' }]
+  // ],
+
+
+  // Global setup and Teardown of tests
+
+  globalSetup: require.resolve('./global-setup'),
+  globalTeardown: require.resolve('./global-teardown'),
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
 
+    // Maximum time each action such as `click()` can take. Defaults to 0 (no limit).
+    // actionTimeout: 0,
+    // Run browser in headless mode.
+    // headless: false,
+    
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://www.ikea.com/in/en/',
+
+    // set up emulation
+    viewport : {width : 1280, height : 720},
+    colorScheme : 'dark',
+
+    // Session storage
+    storageState: '.auth/state.json',
+
+
+
+    // Capture screenshot after each test failure.
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // Record video only when retrying a test for the first time.
+    video: 'on-first-retry'
   },
 
-  timeout : 60000,
+  timeout : 500 * 1000,
 
   /* Configure projects for major browsers */
   projects: [
